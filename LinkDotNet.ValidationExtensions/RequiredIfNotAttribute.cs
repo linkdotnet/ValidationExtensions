842e29ac42656647b2e1914b0e5aead9106f29e5
Steven Giesel <stgiesel35@gmail.com>
2022-02-15 22:08:59 +0100

    Fix whitespace issues and test

--- LinkDotNet.ValidationExtensions/RequiredIfNotAttribute.cs
+++ LinkDotNet.ValidationExtensions/RequiredIfNotAttribute.cs
@@ -1,48 +1,45 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace LinkDotNet.ValidationExtensions;
-
 public class RequiredIfNotAttribute : ValidationAttribute
 {
     private readonly string _propertyName;
     private readonly object? _isNotValue;
-
     public RequiredIfNotAttribute(string propertyName, object? isNotValue)
     {
         _propertyName = propertyName ?? throw new ArgumentNullException(nameof(propertyName));
         _isNotValue = isNotValue;
     }
 
-public override string FormatErrorMessage(string name)
-{
-    var errorMessage = $"Property {name} is required when {_propertyName} is not {_isNotValue}";
-    return ErrorMessage ?? errorMessage;
-}
-
-protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-{
-    ArgumentNullException.ThrowIfNull(validationContext);
-    var property = validationContext.ObjectType.GetProperty(_propertyName);
-
-    if (property == null)
+    public override string FormatErrorMessage(string name)
     {
-        throw new NotSupportedException($"Can't find {_propertyName} on searched type: {validationContext.ObjectType.Name}");
+        var errorMessage = $"Property {name} is required when {_propertyName} is not {_isNotValue}";
+        return ErrorMessage ?? errorMessage;
     }
 
-    var requiredIfNotTypeActualValue = property.GetValue(validationContext.ObjectInstance);
-
-    if (requiredIfNotTypeActualValue == null && _isNotValue != null)
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
+        ArgumentNullException.ThrowIfNull(validationContext);
+        var property = validationContext.ObjectType.GetProperty(_propertyName);
+
+        if (property == null)
+        {
+            throw new NotSupportedException($"Can't find {_propertyName} on searched type: {validationContext.ObjectType.Name}");
+        }
+
+        var requiredIfNotTypeActualValue = property.GetValue(validationContext.ObjectInstance);
+
+        if (requiredIfNotTypeActualValue == null && _isNotValue != null)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (requiredIfNotTypeActualValue == null || !requiredIfNotTypeActualValue.Equals(_isNotValue))
+        {
+            return value == null
+                ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName))
+                : ValidationResult.Success;
+        }
+
         return ValidationResult.Success;
-    }
-
-    if (requiredIfNotTypeActualValue == null)
-    {
-        return value == null
-            ? new ValidationResult(FormatErrorMessage(validationContext.DisplayName))
-            : ValidationResult.Success;
-    }
-
-    return ValidationResult.Success;
     }
 }
