2bacf69ad98df6b1010e78223cf035565c8b0e40
Steven Giesel <stgiesel35@gmail.com>
2022-02-16 11:57:23 +0100

    Added MinLengthIf attributes

---
+++ LinkDotNet.ValidationExtensions/MinLengthIfAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDotNet.ValidationExtensions;
+
+public class MinLengthIfAttribute : MinLengthAttribute
+{
+    private string propertyName;
+    private object? isValue;
+
+    public MinLengthIfAttribute(string propertyName, object? isValue, int length)
+        : base(length)
+    {
+        this.isValue = isValue;
+        this.propertyName = propertyName;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var shouldCheck = validationContext.IsRequired(propertyName, value, false, out var requiredActualValue);
+
+        if (!shouldCheck)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (requiredActualValue == null || requiredActualValue.Equals(isValue))
+        {
+            return value == null
+                ? base.IsValid(value, validationContext)
+                : ValidationResult.Success;
+        }
+
+        return base.IsValid(value, validationContext);
+    }
+}
---
+++ LinkDotNet.ValidationExtensions/MinLengthIfNotAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDotNet.ValidationExtensions;
+
+public class MinLengthIfNotAttribute : MinLengthAttribute
+{
+    private string propertyName;
+    private object? isValue;
+
+    public MinLengthIfNotAttribute(string propertyName, object? isValue, int length)
+        : base(length)
+    {
+        this.isValue = isValue;
+        this.propertyName = propertyName;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var shouldCheck = validationContext.IsRequired(propertyName, value, true, out var requiredActualValue);
+
+        if (!shouldCheck)
+        {
+            return ValidationResult.Success;
+        }
+
+        if (requiredActualValue == null || !requiredActualValue.Equals(isValue))
+        {
+            return value == null
+                ? base.IsValid(value, validationContext)
+                : ValidationResult.Success;
+        }
+
+        return base.IsValid(value, validationContext);
+    }
+}
