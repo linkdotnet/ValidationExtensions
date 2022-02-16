b7170a6c8a73c7b78b1440004d2afbc79a3932bd
Steven Giesel <stgiesel35@gmail.com>
2022-02-16 13:05:09 +0100

    Added MaxLengthIfAttribute

---
+++ LinkDotNet.ValidationExtensions.Tests/MaxLengthIfNotTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace LinkDotNet.ValidationExtensions.Tests;
+
+public class MaxLengthIfNotTests
+{
+    [Fact]
+    public void ShouldNotCheckForMaxIfNotRequired()
+    {
+        var model = new Model("123", false);
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        isValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ShouldCheckWhenConditionIsNotMet()
+    {
+        var model = new Model("123", true);
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+
+        var isValid = Validator.TryValidateObject(model, context, results, true);
+
+        isValid.Should().BeFalse();
+        results.Should().HaveCount(1);
+        results.Single().ErrorMessage.Should().Be("The field 'Name' must be a string or array type with a maximum length of '1' when 'IsRequired' is not 'False'.");
+    }
+
+    public class Model
+    {
+        public Model(string name, bool isRequired)
+        {
+            Name = name;
+            IsRequired = isRequired;
+        }
+
+        [MaxLengthIfNot(nameof(IsRequired), false, 1)]
+        public string Name { get; set; }
+
+        public bool IsRequired { get; set; }
+    }
+}
---
+++ LinkDotNet.ValidationExtensions/MaxLengthIfAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDotNet.ValidationExtensions;
+
+public class MaxLengthIfAttribute : MaxLengthAttribute
+{
+    private readonly string propertyName;
+    private readonly object? isValue;
+
+    public MaxLengthIfAttribute(string propertyName, object? isValue, int length)
+        : base(length)
+    {
+        this.isValue = isValue;
+        this.propertyName = propertyName;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return $"The field '{name}' must be a string or array type with a maximum length of '{Length}' when '{propertyName}' is '{isValue}'.";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var shouldCheck = validationContext.IsRequired(propertyName, isValue, false);
+
+        return !shouldCheck ? ValidationResult.Success : base.IsValid(value, validationContext);
+    }
+}
